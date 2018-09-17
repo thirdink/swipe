cb11b4434ffb78c5c334b6ccc15fd9ea9df22d13
thirdink <rumitwashere@gmail.com>
2018-09-17 21:31:12 +0545

    use creenwodth for rotation

--- src/Deck.js
+++ src/Deck.js
@@ -2,9 +2,11 @@ import React , {Component} from 'react';
 import {
     View,
     Animated,
-    PanResponder
+    PanResponder,
+    Dimensions
 } from 'react-native';
 
+const SCREEN_WIDTH = Dimensions.get('window').width; // retrieve the width of the screen
 class Deck extends Component{
     constructor(props){
         super(props);
@@ -23,19 +25,43 @@ class Deck extends Component{
         
         this.state = { panResponse , position };
     }
+    getCardStyle(){
+        const { position } = this.state;
+        const rotate = position.x.interpolate({
+            inputRange: [-SCREEN_WIDTH * 2,0,SCREEN_WIDTH * 2],
+            outputRange: ['-120deg','0deg','120deg']
+        });
+        return {
+            ...position.getLayout(),
+            transform: [{rotate}]
+
+        };
+    }
     renderCards(){
-        return this.props.data.map(item => {
+        return this.props.data.map((item, index) => {
+            if(index === 0){
+                return (
+                    <Animated.View
+                    key={item.id}
+                    style={this.getCardStyle()}
+            {...this.state.panResponse.panHandlers}
+                    >
+                        {
+                            this.props.renderCard(item)
+                        }
+                    </Animated.View>
+                )
+            }
             return this.props.renderCard(item);
         });
     }
     render(){
         return(
-            <Animated.View 
-            style={this.state.position.getLayout()}
-            {...this.state.panResponse.panHandlers}
+            <View 
+            
             >
                 {this.renderCards()}
-            </Animated.View>
+            </View>
         );
     }
 }
